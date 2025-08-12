c4de072c15893484553b7265c8efc63c857f9bff
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-08-12 02:28:44 +0000

    project setup

---
+++ vetdatahub/settings/settings_prod.py
@@ -0,0 +1,25 @@
+
+import os
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
+ADMINS = [
+    ('Johanan Oppong Amoateng', 'johananoppongamoateng2001@gmail.com'),
+]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
