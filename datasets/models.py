6b84e618b5f0b9c1ea5088d699a077643d7cde4a
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-08-19 22:52:23 +0000

    Refactor dataset and dataset version models: rename fields, update relationships, and add new fields

--- datasets/models.py
+++ datasets/models.py
@@ -28,7 +28,7 @@ class Category(models.Model):
 class Dataset(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    species = models.ForeignKey(Specie, on_delete=models.CASCADE, null=True, blank=True)
+    species = models.ForeignKey(Specie, on_delete=models.CASCADE, null=True, blank=True, related_name='datasets_species')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     license = models.CharField(max_length=255, null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
@@ -42,21 +42,21 @@ class Dataset(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class DatasetVersion(models.Model):
-    dataset = models.ForeignKey(Dataset,on_delete=models.CASCADE)
+    dataset = models.ForeignKey(Dataset,on_delete=models.CASCADE, related_name='dataset_versions')
     version_number = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    file = models.FileField(upload_to='dataset_versions/')
-    format = models.CharField(max_length=100)
-    detailedChanges = models.TextField(null=True, blank=True)
-    breakingChanges = models.BooleanField()
+    file_url = models.URLField()
+    filesize = models.PositiveIntegerField()
+    filetype = models.CharField(max_length=100)
+    changes = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.dataset.title} - Version {self.version_number}"
 
 
 class DatasetRating(models.Model):
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name='dataset_ratings')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[
         MinValueValidator(1),
@@ -64,6 +64,9 @@ class DatasetRating(models.Model):
     ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('dataset', 'user')
 
     def __str__(self):
         return f"{self.user.username} - {self.dataset.title} - {self.rating}"
