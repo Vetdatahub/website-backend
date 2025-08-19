6bb4f437c66347f05ee1a19219b188c992a98773
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-08-19 22:53:42 +0000

    Implement user registration and profile retrieval/update views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from accounts.views import SignUpView, RetrieveProfileView, UpdateProfileView
 
 urlpatterns = [
-    
+    path('register', SignUpView.as_view(), name='register'),
+    path('profile/<str:username>/', RetrieveProfileView.as_view(), name='profile'),
+    path('profile/update/', UpdateProfileView.as_view(), name='profile_update'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from accounts.serializers import RegisterUserSerializer, ProfileSerializer
+from accounts.models import Profile
+from accounts.permissions import IsOwnerOrReadOnly
 # Create your views here.
+
+
+class SignUpView(APIView):
+    serializer_class = RegisterUserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        print("Request data:", request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            data = {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RetrieveProfileView(RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+
+
+
+class UpdateProfileView(UpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+
+
