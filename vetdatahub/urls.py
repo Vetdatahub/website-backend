8d4a3c5a9282f5ae733f0804a8c45018fd00c134
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-08-19 22:53:23 +0000

    Update URL configuration

--- vetdatahub/urls.py
+++ vetdatahub/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth/',include('accounts.urls')),
+    path('api/community/', include('community.urls')),
+    path('api/datasets/', include('datasets.urls')),
 ]
