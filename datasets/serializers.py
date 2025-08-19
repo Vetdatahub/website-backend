68b5c1103d9561ae0e35262ded8cdaba5a03cf20
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2025-08-19 22:53:54 +0000

    Add dataset management views, serializers, and permissions

--- datasets/serializers.py
+++ datasets/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 from datasets.models import Dataset,DatasetRating,DatasetVersion,Specie,Category
 
 class SpecieSerializer(ModelSerializer):
@@ -11,15 +13,64 @@ class CategorySerializer(ModelSerializer):
         model = Category
         fields = '__all__'
 
-class DatasetSerializer(ModelSerializer):
+class CreateDatasetSerializer(ModelSerializer):
+    file_size = serializers.IntegerField(write_only=True, required=True)
+    filetype = serializers.CharField(write_only=True, required=True)
+    version_number = serializers.IntegerField(write_only=True, required=True)
+    changes = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = Dataset
         fields = '__all__'
+
+    def create(self, validated_data):
+        file_url = validated_data.pop('file_url')
+        filetype = validated_data.pop('filetype')
+        version_number = validated_data.pop('version_number')
+        changes = validated_data.pop('changes', None)
+        filesize = validated_data.pop('filesize')
+
+        dataset = Dataset.objects.create(**validated_data)
+        DatasetVersion.objects.create(
+            dataset=dataset,
+            version_number=version_number,
+            file_url=file_url,
+            filesize=filesize,
+            filetype=filetype,
+            changes=changes
+        )
+        return dataset
+
+class DatasetSerializer(ModelSerializer):
+    avg_rating = serializers.SerializerMethodField()
+    versions = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Dataset
+        fields = '__all__'
+
+
+    def get_avg_rating(self, obj):
+        ratings = obj.dataset_ratings.all()
+        if ratings:
+            return sum(rating.rating for rating in ratings) / len(ratings)
+        return 0
+
+    def get_versions(self, obj):
+        return DatasetVersionSerializer(obj.dataset_versions.all(), many=True).data
+    
+    
 
 class DatasetRatingSerializer(ModelSerializer):
     class Meta:
         model = DatasetRating
         fields = "__all__"
+        validators = [
+            UniqueTogetherValidator(
+                queryset=DatasetRating.objects.all(),
+                fields=['dataset', 'user']
+            )
+        ]
 
 class DatasetVersionSerializer(ModelSerializer):
     class Meta:
--- datasets/views.py
+++ datasets/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from datasets.models import Dataset,DatasetRating,DatasetVersion,Specie,Category
+from datasets.serializers import DatasetSerializer, DatasetRatingSerializer, DatasetVersionSerializer, SpecieSerializer, CategorySerializer, CreateDatasetSerializer
+from datasets.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class SpecieListView(ListAPIView):
+    queryset = Specie.objects.all()
+    serializer_class = SpecieSerializer
+    permission_classes = [IsAuthenticated]
+
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class DatasetListView(ListAPIView):
+    queryset = Dataset.objects.all()
+    serializer_class = DatasetSerializer
+    permission_classes = [IsAuthenticated]
+
+class DatasetDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Dataset.objects.all()
+    serializer_class = DatasetSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+
+class DatasetCreateView(CreateAPIView):
+    queryset = Dataset.objects.all()
+    serializer_class = CreateDatasetSerializer
+    permission_classes = [IsAuthenticated]
+
+class DatasetRatingCreateView(CreateAPIView):
+    queryset = DatasetRating.objects.all()
+    serializer_class = DatasetRatingSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class DatasetVersionCreateView(CreateAPIView):
+    queryset = DatasetVersion.objects.all()
+    serializer_class = DatasetVersionSerializer
+    permission_classes = [IsAuthenticated]
+
